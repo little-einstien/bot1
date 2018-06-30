de3af1f15d484beeb32d28756a865f719855b7c2
avsimr <arnavsimer@ymail.com>
2018-06-30 17:32:01 +0530

    New Form for input regarding bimari added

--- src/app/components/stpper-form/stpper-form.component.ts
+++ src/app/components/stpper-form/stpper-form.component.ts
@@ -11,7 +11,7 @@ import { DatepickerOptions } from 'ng2-datepicker';
   styleUrls: ['./stpper-form.component.css']
 })
 export class StpperFormComponent implements OnInit {
-  @Input('data') data;
+  @Input('data') data:any ;
     constructor(private dataHandlerService: DialogflowService,private windowRef : WindowRef) {
     setTimeout(() => {
       M.Datepicker.init(document.querySelectorAll('.datepicker'), {});
