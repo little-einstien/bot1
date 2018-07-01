e64bf8b92f93f3185ebc412fbbee06cc364344ec
avsimr <arnavsimer@ymail.com>
2018-07-01 13:44:51 +0530

    sklhs

--- src/app/components/bot/bot.component.ts
+++ src/app/components/bot/bot.component.ts
@@ -96,6 +96,7 @@ getResponse($event) {
     this.messages.push(
       new Message({ txt: 'Thank you for filling the form', type: 0}, 'assets/images/bot.png', 'bot', new Date())
     );
+    setTimeout(() => { this.scrollToBottom() }, 250);
     return;
   }
 
