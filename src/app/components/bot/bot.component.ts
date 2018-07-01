3e146f840792507481956f9d2828b7730eacf333
avsimr <arnavsimer@ymail.com>
2018-07-01 16:16:29 +0530

    Textual changes

--- src/app/components/bot/bot.component.ts
+++ src/app/components/bot/bot.component.ts
@@ -46,7 +46,7 @@ ngAfterViewInit() {
     if (params.project) {
       this.dialogflowService.project = params.project;
       this.messages.push(
-        new Message({ txt: 'Welcome To DR. GEETA GERA (SKIN, HAIR & LASER CLINIC). I am Pkasy, your AI experience assitant.' , type: 0 }, 'assets/images/bot.png', 'bot', new Date())
+        new Message({ txt: 'Welcome To DR. GEETA GERA (SKIN, HAIR & LASER CLINIC). I am Duffy, your AI experience assitant.' , type: 0 }, 'assets/images/bot.png', 'bot', new Date())
       );
       this.wetherMessage().then(() => {
         this.dialogflowService.getProjectDetails(params.project).then((proj) => {
@@ -172,9 +172,9 @@ wetherMessage() {
         }, 'assets/images/bot.png', 'bot', new Date()))
         console.log(data.data.name)
       }, 500)
-      setTimeout(() => {this.messages.push(new Message({
-        txt: 'Tell me how can i help you today ?', type: 0
-      }, 'assets/images/bot.png', 'bot', new Date()))}, 700);
+      // setTimeout(() => {this.messages.push(new Message({
+      //   txt: 'Tell me how can i help you today ?', type: 0
+      // }, 'assets/images/bot.png', 'bot', new Date()))}, 700);
       resolve();
     })
 
