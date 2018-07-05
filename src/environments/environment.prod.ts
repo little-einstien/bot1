3f4928d09842408a28066a474cc844578b0f8476
Little Einstien <arnavsimer@ymail.com>
2018-07-05 14:43:16 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  nlu_endpoint : 'http://35.200.198.3:4203',
+  bot_endpoint : 'http://35.200.198.3:3000',
+  api_endpoint : 'http://35.200.198.3:3000'
 };
